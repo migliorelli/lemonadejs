3a70f02cf5f64c4a64a2394e91c5728b44aa561d
Paul <you@example.com>
2024-11-30 16:19:39 +0000

    web components, references

---
+++ test/references.js
@@ -0,0 +1,50 @@
+describe('References', () => {
+
+    it('Reference by template', function() {
+        function Test() {
+            return `<div><p>{{this.title}}</p></div>`;
+        }
+
+        lemonade.setComponents({ Test })
+
+        // Get the attributes from the tag
+        function Component() {
+            this.title = true;
+
+            // Title and year are declared in the parent template
+            return `<div>
+                <Test :title="{{this.title}}" />
+            </div>`;
+        }
+
+        // Render the component and assert the return
+        return render(Component).assert('true', function () {
+            let self = this;
+            return self.el.textContent;
+        })
+    });
+
+    it('Boolean test', function() {
+        function Test() {
+            return `<div><p>{{this.title}}</p></div>`;
+        }
+
+        lemonade.setComponents({ Test })
+
+        // Get the attributes from the tag
+        function Component() {
+            this.title = true;
+
+            // Title and year are declared in the parent template
+            return `<div>
+                <Test :title="${true}" />
+            </div>`;
+        }
+
+        // Render the component and assert the return
+        return render(Component).assert('true', function () {
+            let self = this;
+            return self.el.textContent;
+        })
+    });
+});
